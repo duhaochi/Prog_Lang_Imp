52730957a030c234aeb9565a61da562cbc89f9ff
Du haochi <duhaochi@242.187.20.172.s.wireless.uri.edu>
2021-11-30 10:18:58 -0500

    rename type -> types due to error throw by python

---
+++ code/type_s.py
@@ -0,0 +1,5 @@
+# this is where types are defined
+
+# can't name this file types, will give error refer
+# https://stackoverflow.com/questions/43453414/cannot-import-name-mappingproxytype-error-after-importing-functools
+
